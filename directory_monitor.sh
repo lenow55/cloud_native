b80ec3321e1d24a2aa959dfbc01db81fd3041c7b
Новиков Илья <nov.ilucha@yandex.ru>
2023-06-11 18:15:57 +0300

    логирование + протокол логирования

---
+++ directory_monitor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+file_removed() {
+    TIMESTAMP=`date`
+    echo "[$TIMESTAMP]: The file $1$2 was removed from $1"
+}
+
+file_modified() {
+    TIMESTAMP=`date`
+    echo "[$TIMESTAMP]: The file $1$2 was modified"
+    cat $2
+}
+
+file_created() {
+    TIMESTAMP=`date`
+    echo "[$TIMESTAMP]: The file $1$2 was created"
+}
+
+TIMESTAMP=`date`
+echo "[$TIMESTAMP]: start source from $1"
+
+inotifywait -q -m -r -e modify,delete,create $1 | while read DIRECTORY EVENT FILE; do
+    case $EVENT in
+        MODIFY*)
+            file_modified "$DIRECTORY" "$FILE"
+            ;;
+        CREATE*)
+            file_created "$DIRECTORY" "$FILE"
+            ;;
+        DELETE*)
+            file_removed "$DIRECTORY" "$FILE"
+            ;;
+    esac
+done
+

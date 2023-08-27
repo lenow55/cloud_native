7b569befbbd8783b512a10ace09050e75b81e85e
Новиков Илья <nov.ilucha@yandex.ru>
2023-08-27 17:41:55 +0300

    Formatter autopep8 не работает почему-то

--- cross_tests/parser/src/base_logging.py
+++ cross_tests/parser/src/base_logging.py
@@ -7,7 +7,8 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances.keys():
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
